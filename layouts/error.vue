73d30c352346445ca35b09fd56635080116935e8
kthatoto <kthatoto@gmail.com>
2018-07-31 23:53:43 +0900

    Added error component

---
+++ layouts/error.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1 v-if="error.statusCode === 404">Page Not Found</h1>
+    <h1 v-else>Error</h1>
+  </div>
+</template>
+<script>
+export default {
+  props: ['error']
+}
+</script>
