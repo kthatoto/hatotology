f0090f3e9306a18342a212e24db105be58774162
kthatoto <kthatoto@gmail.com>
2018-08-04 01:48:10 +0900

    Added tags header

--- pages/posts/_id.vue
+++ pages/posts/_id.vue
@@ -1,14 +1,19 @@
 <template lang="pug">
 div
-  h2 {{ post.title }}
-  ul
-    li.label(v-for="label in post.labels.nodes") {{ label.name }}
-  vue-markdown(:source="post.body")
+  div(v-if="post.title")
+    h2 {{ post.title }}
+    ul
+      span.label__header tags:
+      li.label(v-for="label in post.labels.nodes") {{ label.name }}
+    p.date Created at: {{ formattedDate(post.createdAt) }}
+    p.date Updated at: {{ formattedDate(post.updatedAt) }}
+    vue-markdown(:source="post.body")
 </template>
 
 <script>
 import post from '~/apollo/queries/post'
 import VueMarkdown from 'vue-markdown'
+import moment from 'moment'
 
 export default {
   components: { VueMarkdown },
@@ -29,6 +34,11 @@ export default {
       },
       update: ({ repository }) => repository.issue
     }
+  },
+  methods: {
+    formattedDate (date) {
+      return moment(date).format('YYYY/MM/DD')
+    }
   }
 }
 </script>
@@ -38,5 +48,15 @@ export default {
   border: 1px solid #aaa;
   padding: 2px 5px;
   border-radius: 5px;
+  color: #444;
+  &__header {
+    color: #444;
+    margin-right: 5px;
+  }
+}
+.date {
+  text-align: right;
+  color: #888;
+  font-size: 0.8rem;
 }
 </style>
